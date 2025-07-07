df8792d7b7075a3eb6088b73bb70353467aaf7f6
Aleix Ramírez <aramirez@opennebula.io>
2025-07-07 14:33:25 +0200

    Implements node volume methods and tests
    Signed-off-by: Aleix Ramírez <aramirez@opennebula.io>

--- pkg/csi/driver/nodeserver_test.go
+++ pkg/csi/driver/nodeserver_test.go
@@ -12,15 +12,17 @@ import (
 )
 
 const (
-	stagingTargetPath = "/mnt" // Example staging target path
+	stagingTargetPath = "/mnt"        // Example staging target path
+	targetPath        = "/mnt/target" // Example target path for publishing
 )
 
-func getTestNodeServer() *NodeServer {
+func getTestNodeServer(mountPoints []string) *NodeServer {
 	driver := &Driver{
 		name:               DefaultDriverName,
 		version:            driverVersion,
 		grpcServerEndpoint: DefaultGRPCServerEndpoint,
 		nodeID:             "test-node-id",
+		maxVolumesPerNode:  30,
 	}
 	commandScriptArray := []testingexec.FakeCommandAction{}
 	//TODO: Simulate real commands
@@ -34,12 +36,15 @@ func getTestNodeServer() *NodeServer {
 			}
 		})
 	}
+	mountPointList := []mount.MountPoint{}
+	for _, mountPoint := range mountPoints {
+		mountPointList = append(mountPointList, mount.MountPoint{
+			Path: mountPoint,
+		})
+	}
+
 	mounter := mount.NewSafeFormatAndMount(
-		mount.NewFakeMounter([]mount.MountPoint{
-			mount.MountPoint{
-				Path: stagingTargetPath,
-			},
-		}), // using fake mounter implementation
+		mount.NewFakeMounter(mountPointList), // using fake mounter implementation
 		&testingexec.FakeExec{
 			CommandScript: commandScriptArray,
 		}, // using fake exec implementation
@@ -50,14 +55,14 @@ func getTestNodeServer() *NodeServer {
 func TestStageVolume(t *testing.T) {
 
 	tcs := []struct {
-		name                   string
-		nodeStageVolumeRequest *csi.NodeStageVolumeRequest
-		expectResponse         *csi.NodeStageVolumeResponse
-		expectError            bool
+		name           string
+		request        *csi.NodeStageVolumeRequest
+		expectResponse *csi.NodeStageVolumeResponse
+		expectError    bool
 	}{
 		{
-			name: "TestBasicMount",
-			nodeStageVolumeRequest: &csi.NodeStageVolumeRequest{
+			name: "[SUCCESS] Test basic volume mount",
+			request: &csi.NodeStageVolumeRequest{
 				VolumeId:          "test-volume-id",
 				StagingTargetPath: stagingTargetPath,
 				VolumeCapability: &csi.VolumeCapability{
@@ -78,8 +83,292 @@ func TestStageVolume(t *testing.T) {
 
 	for _, tc := range tcs {
 		t.Run(tc.name, func(t *testing.T) {
-			ns := getTestNodeServer()
-			response, err := ns.NodeStageVolume(context.Background(), tc.nodeStageVolumeRequest)
+			ns := getTestNodeServer([]string{})
+			response, err := ns.NodeStageVolume(context.Background(), tc.request)
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			if tc.expectResponse != nil {
+				assert.Equal(t, tc.expectResponse, response)
+			} else {
+				assert.NotNil(t, response)
+			}
+		})
+	}
+}
+
+func TestUnstageVolume(t *testing.T) {
+
+	tcs := []struct {
+		name           string
+		request        *csi.NodeUnstageVolumeRequest
+		expectResponse *csi.NodeUnstageVolumeResponse
+		expectError    bool
+	}{
+		{
+			name: "[SUCCESS] Test correct staging target path",
+			request: &csi.NodeUnstageVolumeRequest{
+				VolumeId:          "test-volume-id",
+				StagingTargetPath: stagingTargetPath,
+			},
+			expectResponse: &csi.NodeUnstageVolumeResponse{},
+			expectError:    false,
+		},
+		{
+			name: "[SUCCESS] Test unmounted staging target path",
+			request: &csi.NodeUnstageVolumeRequest{
+				VolumeId:          "test-volume-id",
+				StagingTargetPath: "/mnt/nonexistent",
+			},
+			expectResponse: &csi.NodeUnstageVolumeResponse{},
+			expectError:    false,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			ns := getTestNodeServer([]string{stagingTargetPath})
+			response, err := ns.NodeUnstageVolume(context.Background(), tc.request)
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			if tc.expectResponse != nil {
+				assert.Equal(t, tc.expectResponse, response)
+			} else {
+				assert.NotNil(t, response)
+			}
+		})
+	}
+}
+
+func TestPublishVolume(t *testing.T) {
+
+	tcs := []struct {
+		name           string
+		request        *csi.NodePublishVolumeRequest
+		expectResponse *csi.NodePublishVolumeResponse
+		expectError    bool
+	}{
+		{
+			name: "[SUCCESS] Test correct staging target path",
+			request: &csi.NodePublishVolumeRequest{
+				VolumeId:          "test-volume-id",
+				StagingTargetPath: stagingTargetPath,
+				TargetPath:        targetPath,
+				VolumeCapability: &csi.VolumeCapability{
+					AccessType: &csi.VolumeCapability_Mount{
+						Mount: &csi.VolumeCapability_MountVolume{
+							FsType:     "ext4",
+							MountFlags: []string{"ro"},
+						},
+					},
+				},
+				VolumeContext: map[string]string{
+					"volumeName": "zero",
+				},
+			},
+			expectResponse: &csi.NodePublishVolumeResponse{},
+			expectError:    false,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			ns := getTestNodeServer([]string{stagingTargetPath})
+			response, err := ns.NodePublishVolume(context.Background(), tc.request)
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			if tc.expectResponse != nil {
+				assert.Equal(t, tc.expectResponse, response)
+			} else {
+				assert.NotNil(t, response)
+			}
+		})
+	}
+}
+
+func TestUnpublishVolume(t *testing.T) {
+
+	tcs := []struct {
+		name           string
+		request        *csi.NodeUnpublishVolumeRequest
+		expectResponse *csi.NodeUnpublishVolumeResponse
+		expectError    bool
+	}{
+		{
+			name: "[SUCCESS] Test correct staging target path",
+			request: &csi.NodeUnpublishVolumeRequest{
+				VolumeId:   "test-volume-id",
+				TargetPath: targetPath,
+			},
+			expectResponse: &csi.NodeUnpublishVolumeResponse{},
+			expectError:    false,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			ns := getTestNodeServer([]string{stagingTargetPath})
+			response, err := ns.NodeUnpublishVolume(context.Background(), tc.request)
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			if tc.expectResponse != nil {
+				assert.Equal(t, tc.expectResponse, response)
+			} else {
+				assert.NotNil(t, response)
+			}
+		})
+	}
+}
+
+func TestNodeGetVolumeStats(t *testing.T) {
+
+	tcs := []struct {
+		name           string
+		request        *csi.NodeGetVolumeStatsRequest
+		expectResponse *csi.NodeGetVolumeStatsResponse
+		expectError    bool
+	}{
+		{
+			name: "[ERROR] Test unimplemented",
+			request: &csi.NodeGetVolumeStatsRequest{
+				VolumeId: "test-volume-id",
+			},
+			expectResponse: &csi.NodeGetVolumeStatsResponse{},
+			expectError:    true,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			ns := getTestNodeServer([]string{stagingTargetPath})
+			response, err := ns.NodeGetVolumeStats(context.Background(), tc.request)
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			if tc.expectResponse != nil {
+				assert.Equal(t, tc.expectResponse, response)
+			} else {
+				assert.NotNil(t, response)
+			}
+		})
+	}
+}
+
+func TestNodeExpandVolume(t *testing.T) {
+	tcs := []struct {
+		name           string
+		request        *csi.NodeExpandVolumeRequest
+		expectResponse *csi.NodeExpandVolumeResponse
+		expectError    bool
+	}{
+		{
+			name: "[ERROR] Test unimplemented",
+			request: &csi.NodeExpandVolumeRequest{
+				VolumeId: "test-volume-id",
+			},
+			expectResponse: &csi.NodeExpandVolumeResponse{},
+			expectError:    true,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			ns := getTestNodeServer([]string{stagingTargetPath})
+			response, err := ns.NodeExpandVolume(context.Background(), tc.request)
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			if tc.expectResponse != nil {
+				assert.Equal(t, tc.expectResponse, response)
+			} else {
+				assert.NotNil(t, response)
+			}
+		})
+	}
+}
+
+func TestNodeGetCapabilities(t *testing.T) {
+
+	tcs := []struct {
+		name           string
+		request        *csi.NodeGetCapabilitiesRequest
+		expectResponse *csi.NodeGetCapabilitiesResponse
+		expectError    bool
+	}{
+		{
+			name:    "[Success] Test capabilities",
+			request: &csi.NodeGetCapabilitiesRequest{},
+			expectResponse: &csi.NodeGetCapabilitiesResponse{
+				Capabilities: []*csi.NodeServiceCapability{
+					{
+						Type: &csi.NodeServiceCapability_Rpc{
+							Rpc: &csi.NodeServiceCapability_RPC{
+								Type: csi.NodeServiceCapability_RPC_STAGE_UNSTAGE_VOLUME,
+							},
+						},
+					},
+				},
+			},
+			expectError: false,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			ns := getTestNodeServer([]string{stagingTargetPath})
+			response, err := ns.NodeGetCapabilities(context.Background(), tc.request)
+			if tc.expectError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			if tc.expectResponse != nil {
+				assert.Equal(t, tc.expectResponse, response)
+			} else {
+				assert.NotNil(t, response)
+			}
+		})
+	}
+}
+
+func TestNodeGetInfo(t *testing.T) {
+
+	tcs := []struct {
+		name           string
+		request        *csi.NodeGetInfoRequest
+		expectResponse *csi.NodeGetInfoResponse
+		expectError    bool
+	}{
+		{
+			name:    "[Success] Test retrieved node info",
+			request: &csi.NodeGetInfoRequest{},
+			expectResponse: &csi.NodeGetInfoResponse{
+				NodeId:            "test-node-id",
+				MaxVolumesPerNode: 30,
+			},
+			expectError: false,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			ns := getTestNodeServer([]string{stagingTargetPath})
+			response, err := ns.NodeGetInfo(context.Background(), tc.request)
 			if tc.expectError {
 				assert.Error(t, err)
 			} else {
